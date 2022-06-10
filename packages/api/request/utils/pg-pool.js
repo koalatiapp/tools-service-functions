729cbf570f02c7b59c55240a6c87ba313b5c7c14
Émile Perron <contact@emileperron.com>
2022-06-09 20:09:06 -0400

    fix: add timeouts for postgres queries and clients

--- packages/api/request/utils/pg-pool.js
+++ packages/api/request/utils/pg-pool.js
@@ -21,17 +21,19 @@ let poolInstance = null;
 module.exports = () => {
 	if (!poolInstance) {
 		const sslCAContents = getCACertificateContents();
+		const poolConfig = {
+			statement_timeout: 10000,
+			idle_in_transaction_session_timeout: 30000,
+		};
 
 		if (sslCAContents) {
-			poolInstance = new Pool({
-				ssl: {
-					rejectUnauthorized: false,
-					ca: sslCAContents,
-				},
-			});
-		} else {
-			poolInstance = new Pool();
+			poolConfig.ssl = {
+				rejectUnauthorized: false,
+				ca: sslCAContents,
+			};
 		}
+
+		poolInstance = new Pool(poolConfig);
 	}
 
 	return poolInstance;
